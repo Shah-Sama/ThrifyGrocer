e11f178a5ac5af83dfac4d63701901696d52c886
Shaheer Arfat <65451162+Shah-Sama@users.noreply.github.com>
2023-12-08 18:55:28 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Your JavaScript code goes here
+
+// Example: Add item to basket
+function addToBasket(itemId, itemName, itemPrice) {
+    // Logic to add item to the basket
+}
+
+// Example: Remove item from basket
+function removeFromBasket(itemId) {
+    // Logic to remove item from the basket
+}
+
+// Example: Checkout
+function checkout() {
+    // Logic to process the checkout
+}
