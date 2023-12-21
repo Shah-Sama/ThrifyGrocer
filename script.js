9c854ac6a99c3a279536700c05c15604fe6caa53
Shaheer Arfat <65451162+Shah-Sama@users.noreply.github.com>
2023-12-20 20:07:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function removeFromBasket(itemId) {
 function checkout() {
     // Logic to process the checkout
 }
+
+document.getElementById('menu-button').addEventListener('click', function() {
+    var nav = document.getElementById('nav');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+});
