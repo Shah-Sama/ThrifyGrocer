68f77591c64ce04deaafff0837c52cf77ecf6ffd
Shaheer Arfat <65451162+Shah-Sama@users.noreply.github.com>
2023-12-25 12:59:29 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,10 +1,46 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "log"
     "net/http"
+    "net/smtp"
 )
+
+
+type EmailContent struct {
+    Name    string `json:"name"`
+    Email   string `json:"email"`
+    Message string `json:"message"`
+}
+
+func main() {
+    http.HandleFunc("/send-email", sendEmailHandler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func sendEmailHandler(w http.ResponseWriter, r *http.Request) {
+    var email EmailContent
+    err := json.NewDecoder(r.Body).Decode(&email)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    sendEmail(email)
+    fmt.Fprintf(w, "Email sent successfully")
+}
+
+func sendEmail(emailContent EmailContent) {
+    // Set up authentication information.
+    auth := smtp.PlainAuth("", "your-email@gmail.com", "your-password", "smtp.gmail.com")
+
+    to := []string{"shaheerarfat111@gmail.com"}
+    msg := []byte("To: shaheerarfat111@gmail.com\r\n" +
+        "Subject: Contact Form Submission\r\n" +
+       
+
 
 func main() {
     http.HandleFunc("/submit-grocery-form", handleGroceryForm)
